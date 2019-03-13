cd080b84eda77a45151bd1a82a005de1c4066f16
favorable-vector-1743 <epzads@gmail.com>
2019-03-13 18:39:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,45 +2,5 @@
 
 class Shoe 
 
-#attr_accessor
-#:color
 
-def brand
-
-@brand 
-end   
-
-def color = (color = "red")
-
-@color = color   
-end   
-
-def size=(num)
-  
-@size = num 
-end 
-
-def material=(material)
-  
-@material = material  
-end   
-
-
-def condition=(new)
-  
-@condition = new   
-end 
-
-def cobble
-  
-  puts "that the shoe has been repaired"
-  
-end 
-
-def initialize(brand)
-
-@brand = brand  
-end   
-  
-  
 end   
